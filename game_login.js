457e55e6d518938727d518d004f80b1e46338857
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-08 12:48:55 -0300

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -1,11 +1,11 @@
 
-function addUser() {
-  player1_name = document.getElementById("player1_name_input").value;
-  player2_name = document.getElementById("player2_name_input").value;
+//Passo 1:Crie uma função que chama addUser
 
-    localStorage.setItem("player1_name", player1_name);
-    localStorage.setItem("player2_name", player2_name);
+//Passo 2:Dentro dessa função utilize o códio a seguir para pegar o nome do primeiro jogador
+//player1_name = document.getElementById("player1_name_input").value;
+//Faça o mesmo para pegar o nome do segundo jogador
 
-    window.location = "game_page.html";
-}
-
+//Passo 3:Utilize o código a seguir para enviar o nome do primeiro jogador ao localStorage(Armazenamento local)
+//localStorage.setItem("player2_name", player2_name);
+//Faça o mesmo para enviar o nome do segundo jogador
+//Utilize o código abaixo para alterar para a segunda tela
